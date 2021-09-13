b7225c5efb46583941b9591c79ba2de5204337c1
ScepticalGoon <89743950+ScepticalGoon@users.noreply.github.com>
2021-09-13 23:17:48 +0600

    Initial commit

---
+++ message.php
@@ -0,0 +1,82 @@
+<form>
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Name" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Message.." required></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button type="submit">Send message</button>
+                        </div>
+                    </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    <!-- footer -->
+    <section id="footer">
+        <img src="Images/S.G.jpg" class="footer-img">
+        <div class="title-text">
+            <p>Contact</p>
+            <h1>Visit Us Today</h1>
+        </div>
+        <div class="footer-row">
+            <div class="footer-left">
+                <h1>Opening Hours</h1>
+                <p> <i class="fas fa-clock"></i> Monday to Friday - 9am to 9pm</p>
+                <p> <i class="fas fa-clock"></i> Saturday and Sunday - 8am to 11pm</p>
+            </div>
+            <div class="footer-right">
+                <h1>Get In Touch</h1>
+                <p>Kushtia,Bangladesh <i class="fas fa-map-marker-alt"></i> </p>
+                <p>arianfahim12@gmail.com <i class="fas fa-envelope-square"></i></p>
+                <p>+880 172847149 <i class="fas fa-phone"></i> </p>
+            </div>
+        </div>
+        <div class="social-links">
+            <a href="https://www.facebook.com/Sceptical-Goon-106441291441243" target="_blank">
+            <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="https://www.instagram.com/arianfahim07/" target="_blank">
+            <i class="fab fa-instagram"></i>
+            </a>
+            <a href="https://discord.gg/GNKZDKX7mJ" target="_blank">
+            <i class="fab fa-discord"></i>
+            </a>
+            <a href="https://www.youtube.com/channel/UCZbukGaPM2sPadncy_SnvWg" target="_blank">
+            <i class="fab fa-youtube"></i>
+            </a>
+            <p>Copyright © 2021 All rights reserved Sceptical Goon. </p>
+            <h6>Last Updated on 22 July 2021 4:14PM</h6>
+        </div>
+		
+		
+
+    </section>
